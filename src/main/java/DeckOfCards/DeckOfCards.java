060bb6ed44544264b5ccf9f597c18d67784a7a2e
Karo Mahmoud <karofm@stud.ntnu.no>
2022-03-13 15:34:48 +0100

    Implemented the class DeckOfCards and its constructor and attributes.

---
+++ src/main/java/DeckOfCards/DeckOfCards.java
@@ -0,0 +1,15 @@
+package DeckOfCards;
+
+import PlayingCard.PlayingCard;
+
+import java.util.ArrayList;
+
+public class DeckOfCards {
+    private ArrayList<PlayingCard> deck;
+    private final char[] suit = { 'S', 'H', 'D', 'C' };
+
+    public DeckOfCards(ArrayList<PlayingCard> deck){
+    deck=this.deck;
+    }
+
+}
