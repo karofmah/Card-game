2afecb5ee86c1fb8c1f64926d29cc75898f933f5
Karo Mahmoud <karofm@stud.ntnu.no>
2022-03-16 16:17:15 +0100

    Added methods in PlayerHand.java.

--- src/main/java/PlayerHand/PlayerHand.java
+++ src/main/java/PlayerHand/PlayerHand.java
@@ -3,28 +3,38 @@ package PlayerHand;
 import PlayingCard.PlayingCard;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlayerHand {
-    public int numberOfCards;
     public ArrayList<PlayingCard> hand;
 
     public PlayerHand(ArrayList<PlayingCard> hand){
         this.hand=hand;
     }
 
-    public int getNumberOfCards() {
-        return numberOfCards;
-    }
-
     public ArrayList<PlayingCard> getHand() {
         return hand;
+    }
+    public int sumOfTheFaces() {
+        int sumOfFaces= hand.stream().map(PlayingCard::getFace).reduce((a,b)->a+b).get();
+        return sumOfFaces;
+    }
+    public List<PlayingCard> cardOfHearts(){
+        List<PlayingCard> cardsOfHearts= hand.stream().filter(p->p.getSuit()=='H').collect(Collectors.toList());
+        return cardsOfHearts;
+    }
+    public boolean queenOfSpades(){
+        return hand.stream().anyMatch(p -> p.getSuit()=='S'&& p.getFace()== 12);
+    }
+    public boolean flush(){
+        return hand.stream().allMatch(s->s.getSuit()=='S'||s.getSuit()=='H'||s.getSuit()=='D'||s.getSuit()=='C');
     }
 
     @Override
     public String toString() {
         return "PlayerHand{" +
-                "numberOfCards=" + numberOfCards +
-                ", hand=" + hand +
+                "hand=" + hand +
                 '}';
     }
 }
