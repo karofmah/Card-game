96e3c77d4a1c339006bc03fdbc638389e619089e
Karo Mahmoud <karofm@stud.ntnu.no>
2022-03-17 13:31:13 +0100

    Did changes in display name in DeckOfCardsTest.java.

--- src/test/java/DeckOfCardsTest.java
+++ src/test/java/DeckOfCardsTest.java
@@ -2,6 +2,7 @@ import DeckOfCards.DeckOfCards;
 import HandOfCards.HandOfCards;
 import PlayingCard.PlayingCard;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class DeckOfCardsTest {
@@ -10,18 +11,21 @@ public class DeckOfCardsTest {
     private HandOfCards hand;
 
     @BeforeEach
+    @DisplayName("Fills deck with cards and deals hand")
     public void setup(){
         this.deck=new DeckOfCards();
         this.hand=new HandOfCards(deck.dealHand(5));
     }
 
     @Test
+    @DisplayName("Checks if the deck will lose, and the hand will gain, 5 cards when 5 cards are dealt")
     public void cardsAreDealtProperly(){
         assertEquals(47, deck.getSize());
         assertEquals(5, hand.getSize());
     }
 
     @Test
+    @DisplayName("Calculates the sum of faces in a hand ")
     public void calculateSumOfTheFaces() {
         deck.dealHand(5);
         int expectedSumOfFaces = 0;
@@ -31,14 +35,16 @@ public class DeckOfCardsTest {
         assertEquals(expectedSumOfFaces, hand.sumOfTheFaces());
     }
     @Test
-    public void representCardOfHearts(){
+    @DisplayName("Checks if the list that represents cards of hearts, only contains cards with suit of hearts")
+    public void checkCardOfHearts(){
         deck.dealHand(5);
         for (PlayingCard card:hand.cardOfHearts()) {
             assertEquals('H',card.getSuit());
         }
     }
     @Test
-    public void queenOfSpadesExists(){
+    @DisplayName("Checks if queen of spades exists in a hand ")
+    public void checkIfQueenOfSpadesExists(){
         for (PlayingCard card:hand.getHand()) {
             if(card.getSuit()=='S' && card.getFace()==12){
                 assertTrue(hand.queenOfSpades());
@@ -47,6 +53,5 @@ public class DeckOfCardsTest {
             }
         }
     }
-
 
 }
